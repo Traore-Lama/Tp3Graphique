5ec72ab49f34e83dd7e0cba896f0a3c8fc60c4c6
Traore-Lama <traorelama997@gmail.com>
2026-02-09 13:21:51 +0100

    Tp 3 exo 1 fini (2)

--- src/main/java/graphique/tp3graphique/Tp3Controller.java
+++ src/main/java/graphique/tp3graphique/Tp3Controller.java
@@ -39,35 +39,35 @@ public class Tp3Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
-//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//        alert.setTitle("Information");
-//        alert.setHeaderText(null);
-//
-//        Realisateur realisateur = new Realisateur("Spielberg", "Steven");
-//        alert.setContentText(
-//                "Nom du realisateur : " + realisateur.getNom() +
-//                "\n" + "Prenom du realisateur : " + realisateur.getPrenom()
-//        );
-//        alert.showAndWait();
-//
-//        Acteur acteur1 = new Acteur("Magimel", "Benoit");
-//        Acteur acteur2 = new Acteur("Deneuve", "Catherine");
-//        Acteur acteur3 = new Acteur("De France", "Cecile");
-//
-//
-//        Film film = new Film("De son Vivant", 1345681, 2021, realisateur);
-//        alert.setContentText(
-//                "Titre du film : " + film.getTitre() +
-//                 "\n" + "Nombre d'entrees : " + film.getNbEntree() +
-//                 "\n" + "Annee de sortie : " + film.getAnneeSortie() +
-//                 "\n" + "Nom du realisateur : " + realisateur.getNom() +
-//                 "\n" + "Prenom du realisateur : " + realisateur.getPrenom() +
-//                 "\n" + "Liste des acteurs " + "\n" + "Nom de l'acteur = " + acteur1.getNomActeur() + "\n" +
-//                  "Prenom de l'acteur = " + acteur1.getPrenomActeur() + "\n" + "Nom de l'acteur = " + acteur2.getNomActeur() + "\n" +
-//                        "Prenom de l'acteur = " + acteur2.getPrenomActeur() + "\n" + "Nom de l'acteur = " + acteur3.getNomActeur() + "\n" +
-//                 "Prenom de l'ateur = " + acteur3.getPrenomActeur()
-//        );
-//        alert.showAndWait();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText(null);
+
+        Realisateur realisateur = new Realisateur("Spielberg", "Steven");
+        alert.setContentText(
+                "Nom du realisateur : " + realisateur.getNom() +
+                "\n" + "Prenom du realisateur : " + realisateur.getPrenom()
+        );
+        alert.showAndWait();
+
+        Acteur acteur1 = new Acteur("Magimel", "Benoit");
+        Acteur acteur2 = new Acteur("Deneuve", "Catherine");
+        Acteur acteur3 = new Acteur("De France", "Cecile");
+
+
+        Film film = new Film("De son Vivant", 1345681, 2021, realisateur);
+        alert.setContentText(
+                "Titre du film : " + film.getTitre() +
+                 "\n" + "Nombre d'entrees : " + film.getNbEntree() +
+                 "\n" + "Annee de sortie : " + film.getAnneeSortie() +
+                 "\n" + "Nom du realisateur : " + realisateur.getNom() +
+                 "\n" + "Prenom du realisateur : " + realisateur.getPrenom() +
+                 "\n" + "Liste des acteurs " + "\n" + "Nom de l'acteur = " + acteur1.getNomActeur() + "\n" +
+                  "Prenom de l'acteur = " + acteur1.getPrenomActeur() + "\n" + "Nom de l'acteur = " + acteur2.getNomActeur() + "\n" +
+                        "Prenom de l'acteur = " + acteur2.getPrenomActeur() + "\n" + "Nom de l'acteur = " + acteur3.getNomActeur() + "\n" +
+                 "Prenom de l'ateur = " + acteur3.getPrenomActeur()
+        );
+        alert.showAndWait();
 
 
         // Remplissements des Colones du TableView
@@ -84,13 +84,13 @@ public class Tp3Controller implements Initializable {
         Realisateur realisateur3 = new Realisateur("Guédiguian", "Robert");
 
         // Les 7 acteurs
-        Acteur acteur1 = new Acteur("Magimel", "Benoit");
-        Acteur acteur2 = new Acteur("Deneuve", "Catherine");
-        Acteur acteur3 = new Acteur("De France", "Cécile");
-        Acteur acteur4 = new Acteur("Chalamet", "Timothée");
-        Acteur acteur5 = new Acteur("Elle", "Fanning");
-        Acteur acteur6 = new Acteur("Ascaride", "Arianne");
-        Acteur acteur7 = new Acteur("Darroussin", "Jean-Pierre");
+        Acteur acteur4 = new Acteur("Magimel", "Benoit");
+        Acteur acteur5 = new Acteur("Deneuve", "Catherine");
+        Acteur acteur6 = new Acteur("De France", "Cécile");
+        Acteur acteur7 = new Acteur("Chalamet", "Timothée");
+        Acteur acteur8 = new Acteur("Elle", "Fanning");
+        Acteur acteur9 = new Acteur("Ascaride", "Arianne");
+        Acteur acteur10 = new Acteur("Darroussin", "Jean-Pierre");
 
         // Les 3 films
         Film film1 = new Film("De son vivant", 745239, 2021, realisateur1);
@@ -98,15 +98,15 @@ public class Tp3Controller implements Initializable {
         Film film3 = new Film("La pie voleuse", 871425, 2025, realisateur3);
 
         // On ajoute les acteurs à chaque film
-        film1.ajouterActeur(acteur1);
-        film1.ajouterActeur(acteur2);
-        film1.ajouterActeur(acteur3);
+        film1.ajouterActeur(acteur4);
+        film1.ajouterActeur(acteur5);
+        film1.ajouterActeur(acteur6);
 
-        film2.ajouterActeur(acteur4);
-        film2.ajouterActeur(acteur5);
+        film2.ajouterActeur(acteur7);
+        film2.ajouterActeur(acteur8);
 
-        film3.ajouterActeur(acteur6);
-        film3.ajouterActeur(acteur7);
+        film3.ajouterActeur(acteur9);
+        film3.ajouterActeur(acteur10);
 
         // Une collection pour stocker tous les films
         List<Film> films = new ArrayList<>();
